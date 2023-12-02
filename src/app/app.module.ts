a47ba5f4bf01df119ca3f025d5171d3a1b1f84e5
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2023-12-02 14:36:18 +0100

    U3S2G5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { CompletatiComponent } from './completati/completati.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    CompletatiComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
